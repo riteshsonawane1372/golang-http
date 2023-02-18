2676c36289006e4b5d7f6ef2d7012dbb679b0e61
riteshsonawane1372 <riteshsonawane1372@gmail.com>
2023-02-18 18:51:28 +0530

    completed

--- internal/main.go
+++ internal/main.go
@@ -1,28 +1,90 @@
-package main 
+package main
 
-import
-(
-  "fmt"
-  "html"
+import (
+
   "log"
-  "net/http"
+	"net/http"
+
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/riteshsonawane/http-restAPI/pkg/swagger/server/restapi"
+	"github.com/riteshsonawane/http-restAPI/pkg/swagger/server/restapi/operations"
 )
 
 func main(){
 
-	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
-		
-		fmt.Fprintf(w,"Hello %q",html.EscapeString(r.URL.Path))
 
-	})
+    //Init Swagger
 
-  log.Println("Port 8000 Running")
+    swagger,err:=loads.Analyzed(restapi.SwaggerJSON,"")
+    if err!=nil{
+      log.Fatal(err)
+    }
 
-  log.Fatal(http.ListenAndServe(":8000",nil))
+    api:=operations.NewHelloAPIAPI(swagger)
+    server:=restapi.NewServer(api)
+
+    defer func(){
+        err:=server.Shutdown()
+        if err!=nil{
+          log.Fatal(err)
+        }
+    }()
+
+    server.Port=8000
+
+  
+  
+    api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
+
+    api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHelloUser)
+
+    api.GetGopherNameHandler = operations.GetGopherNameHandlerFunc(GetGopherByName)
+
+    // Start server 
+    if err := server.Serve(); err != nil {
+        log.Fatalln(err)
+    }
+}
+
+//Health route returns OK
+func Health(operations.CheckHealthParams) middleware.Responder {
+    return operations.NewCheckHealthOK().WithPayload("OK")
+}
+
+//Returns Hello + UserName
+func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
+  return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
+}
+
+
+func GetGopherByName(gopher operations.GetGopherNameParams) middleware.Responder{
+
+  var URL string
+
+  if gopher.Name!=""{
+    URL="https://github.com/scraly/gophers/raw/main/" + gopher.Name + ".png"
+  }else{
+    URL = "https://github.com/scraly/gophers/raw/main/dr-who.png"
+  }
+
+  response,err:=http.Get(URL)
+  if err!=nil{
+    log.Fatal(err)
+  }
+
+  return operations.NewGetGopherNameOK().WithPayload(response.Body)
+
+}
+
+
+
+
+
 
   
 
-}
+
 
 
 
