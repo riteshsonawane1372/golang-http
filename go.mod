b13a194f87872bdf4d004c4c40842633e57a3a9c
riteshsonawane1372 <riteshsonawane1372@gmail.com>
2023-02-18 18:36:35 +0530

    Swagger API

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/riteshsonawane/http-restAPI
+
+go 1.19
---
+++ internal/main.go
@@ -0,0 +1,28 @@
+package main 
+
+import
+(
+  "fmt"
+  "html"
+  "log"
+  "net/http"
+)
+
+func main(){
+
+	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
+		
+		fmt.Fprintf(w,"Hello %q",html.EscapeString(r.URL.Path))
+
+	})
+
+  log.Println("Port 8000 Running")
+
+  log.Fatal(http.ListenAndServe(":8000",nil))
+
+  
+
+}
+
+
+
